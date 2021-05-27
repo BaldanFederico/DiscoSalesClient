c98c870e1ec55e5558268e9c1b2fd6ed5bbdb457
manue <manue@DESKTOP-D4E17EE.homenet.telecomitalia.it>
2021-05-27 23:15:30 +0200

    Renamed classes and cleaned source codes

---
+++ src/nuovoclient/Utente.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuovoclient;
+
+/**
+ *
+ * @author agostinelli.luca
+ */
+public class Utente {
+    private String nome;
+    private String password;
+    private String email;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String Email) {
+        this.email = Email;
+    }
+}
